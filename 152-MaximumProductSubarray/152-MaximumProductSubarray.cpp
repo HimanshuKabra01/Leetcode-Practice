f8d734c288dd90c432330102342685245cf2f036
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-06-07 17:39:31 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.cpp
@@ -0,0 +1,29 @@
+// Last updated: 6/7/2025, 5:39:29 PM
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int currProduct = 1;
+        int maxProduct = INT_MIN;
+        
+        for(int i = 0; i < n; i++) {
+            currProduct *= nums[i];
+            maxProduct = max(currProduct, maxProduct);
+            
+            if(currProduct == 0) {
+                currProduct = 1;
+            }
+        }
+        currProduct = 1;
+        for(int i = n-1; i >= 0; i--) {
+            currProduct *= nums[i];
+            maxProduct = max(currProduct, maxProduct);
+
+            if(currProduct == 0) {
+                currProduct = 1;
+            }
+        }
+
+        return maxProduct;
+    }
+};
