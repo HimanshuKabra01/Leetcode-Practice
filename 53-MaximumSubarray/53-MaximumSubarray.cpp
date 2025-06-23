990cd83470f9944ba4029e102fc4193b5a02041d
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-06-24 00:07:11 +0530

    File updated at 6/24/2025, 12:07:11 AM

--- 53-MaximumSubarray/53-MaximumSubarray.cpp
+++ 53-MaximumSubarray/53-MaximumSubarray.cpp
@@ -1,16 +1,20 @@
-// Last updated: 6/7/2025, 5:39:37 PM
+// Last updated: 6/24/2025, 12:07:11 AM
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int currSum = 0;
         int maxSum = INT_MIN;
+
         for(int val : nums) {
             currSum += val;
+
             maxSum = max(currSum, maxSum);
+
             if(currSum < 0) {
                 currSum = 0;
             }
         }
-        return maxSum;
+
+        return maxSum;  
     }
 };
