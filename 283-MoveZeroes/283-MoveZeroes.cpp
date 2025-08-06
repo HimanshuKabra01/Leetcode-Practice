6d6463ec31d2904c5cd1e017e9dce578b45fecf1
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-08-06 20:50:01 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/6/2025, 8:50:00 PM
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int pos = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) {
+                nums[pos] = nums[i];
+                pos++;
+            }
+        }
+
+        while(pos < n) {
+            nums[pos] = 0;
+            pos++;
+        }
+    }
+};
