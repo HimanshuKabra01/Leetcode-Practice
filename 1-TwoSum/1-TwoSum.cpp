311f53b4aca6b0ffc2b9871cbbca95773b437270
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-08-09 16:13:38 +0530

    File updated at 8/9/2025, 4:13:37 PM

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,16 +1,37 @@
-// Last updated: 6/7/2025, 5:39:46 PM
+// Last updated: 8/9/2025, 4:13:37 PM
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // vector<int> ans;
+        // int n = nums.size();
+
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i + 1; j < n; j++) {
+        //         if(nums[i] + nums[j] == target) {
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+
+        // return ans;
+
+        unordered_map<int, int> m;
         vector<int> ans;
+
         for(int i = 0; i < nums.size(); i++) {
-            for(int j = i+1; j < nums.size(); j++) {
-                if(nums[i]+nums[j] == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+            int first = nums[i];
+            int sec = target - first;
+
+            if(m.find(sec) != m.end()) {
+                ans.push_back(i);
+                ans.push_back(m[sec]);
+                break;
             }
+
+            m[first] = i;
         }
+
         return ans;
     }
 };
