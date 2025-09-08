d20d3f063aa6500fbdc1d41a5968effa19fb956d
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-09-08 19:54:15 +0530

    File updated at 9/8/2025, 7:54:15 PM

--- 50-Pow(x,n)/50-Pow(x,n).cpp
+++ 50-Pow(x,n)/50-Pow(x,n).cpp
@@ -1,21 +1,59 @@
-// Last updated: 6/25/2025, 11:46:15 PM
+// Last updated: 9/8/2025, 7:54:15 PM
 class Solution {
 public:
-    double myPow(double x, int n) {
-        long binForm = n;
-        if(binForm < 0) {
-            x = 1/x;
-            binForm = -binForm;
-        }
-        double ans = 1;
-
-        while (binForm > 0) {
-            if(binForm % 2 == 1) {
-                ans *= x;
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+        //horizontal
+        for(int j = 0; j < n; j++) {
+            if(board[row][j] == 'Q') {
+                return false;
             }
-            x *= x;
-            binForm /= 2;
         }
+
+        //vertical
+        for(int i = 0; i < n; i++) {
+            if(board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        //left diagonal
+        for(int i = row, j = col; i >= 0 && j >= 0; i--,j--) {
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        //right diagonal
+        for(int i = row, j = col; i >= 0 && j < n; i--,j++) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void nQueens(vector<string>& board, vector<vector<string>>& ans, int row, int n) {
+        if(row == n) {
+            ans.push_back({board});
+            return;
+        }
+
+        for(int j = 0; j < n; j++) {
+            if(isSafe(board, row, j, n)) {
+                board[row][j] = 'Q';
+                nQueens(board, ans, row+1, n);
+                board[row][j] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n , string(n, '.'));
+        vector<vector<string>> ans;
+
+        nQueens(board, ans, 0, n);
+
         return ans;
     }
 };
