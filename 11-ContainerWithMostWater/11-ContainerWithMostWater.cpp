7955a4cb81eb1c105a73644bf297369aef482fc0
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-10-04 11:20:57 +0530

    File updated at 10/4/2025, 11:20:57 AM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -1,29 +1,26 @@
-// Last updated: 7/15/2025, 8:54:24 PM
+// Last updated: 10/4/2025, 11:20:57 AM
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        //  BRUTE APPROACH
-        // int currArea, maxArea = 0;
-        // for(int i = 0; i < n; i++) {
-        //     for(int j = i+1; j < n; j++) {
-        //         currArea = min(height[i], height[j])*(j-i);
-        //         maxArea = max(currArea, maxArea);
-        //     }
-        // }
-        // return maxArea;
-        int i = 0, j = n - 1;
-        int maxArea = 0;
-        while (i < j) {
-            int ht = min(height[i], height[j]);
-            int currArea = ht*(j-i);
-            maxArea = max(maxArea, currArea);
-            if(height[j] > height[i]) {
-                i++;
-            } else {
-                j--;
-            }
+      int n = height.size();
+
+      int i = 0, j = n - 1;
+      int maxArea = 0;
+
+      while(i < j) {
+        int ht = min(height[i], height[j]);
+        int width = j - i;
+        int currArea = ht*width;
+
+        maxArea = max(maxArea, currArea);
+
+        if(height[j] > height[i]) {
+            i++;
+        } else {
+            j--;
         }
-        return maxArea;
+      } 
+
+      return maxArea; 
     }
 };
