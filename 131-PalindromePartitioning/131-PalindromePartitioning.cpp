fa51a48cff213d69819e6f75ad2966ba7c0ac779
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-01-01 01:41:21 +0530

    File updated at 1/1/2026, 1:41:22 AM

--- 131-PalindromePartitioning/131-PalindromePartitioning.cpp
+++ 131-PalindromePartitioning/131-PalindromePartitioning.cpp
@@ -1,42 +1,49 @@
-// Last updated: 10/5/2025, 11:47:28 AM
-class Solution {
-public:
-    bool isValid(string s) {
-        string s2 = s;
-
-        reverse(s2.begin(), s2.end());
-
-        if(s2 == s) {
-            return true;
-        }
-
-        return false;
-    }
-
-    void getAllPart(string s, vector<vector<string>>& ans, vector<string>& partitions){
-        if(s.size() == 0) {
-            ans.push_back({partitions});
-        }
-
-        for(int i = 0; i < s.size(); i++) {
-            string part = s.substr(0, i+1);
-
-            if(isValid(part)){
-                partitions.push_back(part);
-
-                getAllPart(s.substr(i+1), ans, partitions);
-
-                partitions.pop_back();
-            }
-        }
-    }
-
-    vector<vector<string>> partition(string s) {
-       vector<vector<string>> ans;
-       vector<string> partitions;
-
-       getAllPart(s, ans, partitions);
-
-       return ans;
-    }
-};
+// Last updated: 1/1/2026, 1:41:22 AM
+1class Solution {
+2public:
+3
+4    bool helper(vector<vector<char>>& board, string word, int row, int col, int idx) {
+5        int m = board.size();
+6        int n = board[0].size();
+7
+8        if(idx == word.size()) {
+9            return true;
+10        }
+11
+12        //base case
+13        if(row >= m || col >= n || row < 0 || col < 0 || board[row][col] != word[idx] || board[row][col] == '#') {
+14            return false;
+15        }
+16
+17        char temp = board[row][col];
+18        board[row][col] = '#';
+19
+20        bool found = false;
+21
+22        if(helper(board, word, row, col+1, idx+1) || helper(board, word, row, col-1, idx   +1) || helper(board, word, row+1, col, idx+1) || helper(board, word, row-1, col, idx+1)) {
+23            found = true;
+24        }
+25
+26        //backtracking
+27        board[row][col] = temp;
+28
+29        return found;
+30    }
+31
+32    bool exist(vector<vector<char>>& board, string word) {
+33        int m = board.size();
+34        int n = board[0].size();
+35
+36        int first = word[0];
+37
+38        for(int i = 0; i < m; i++) {
+39            for(int j = 0; j < n; j++) {
+40                if(board[i][j] == first) {
+41                    if(helper(board, word, i, j, 0)) return true;
+42                }
+43            }
+44        }
+45
+46        return false;
+47    }
+48};
