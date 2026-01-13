feaaeb98be2da5a79d6518aa7fc37410f91fa70d
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-01-13 19:47:20 +0530

    File updated at 1/13/2026, 7:47:20 PM

--- 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.cpp
+++ 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/13/2026, 7:40:28 PM
+// Last updated: 1/13/2026, 7:47:20 PM
 1class MyQueue {
 2public:
 3    stack<int> st1;
@@ -8,43 +8,38 @@
 7    }
 8    
 9    void push(int x) {
-10        while(!st1.empty()){
-11            st2.push(st1.top());
-12            st1.pop();
-13        }
-14
-15        st1.push(x);
-16
-17        while(!st2.empty()) {
-18            st1.push(st2.top());
-19            st2.pop();
-20        }
-21    }
-22    
-23    int pop() {
-24        if(empty()) return -1;
-25
-26        int val = st1.top();
-27        st1.pop();
-28        return val;
-29    }
-30    
-31    int peek() {
-32        if(empty()) return -1;
-33
-34        return st1.top();
-35    }
-36    
-37    bool empty() {
-38        return st1.size() == 0;
-39    }
-40};
-41
-42/**
-43 * Your MyQueue object will be instantiated and called as such:
-44 * MyQueue* obj = new MyQueue();
-45 * obj->push(x);
-46 * int param_2 = obj->pop();
-47 * int param_3 = obj->peek();
-48 * bool param_4 = obj->empty();
-49 */
+10        st1.push(x);
+11    }
+12    
+13    int pop() {
+14        if(empty()) return -1;
+15
+16        int val = peek();
+17        st2.pop();
+18        return val;
+19    }
+20    
+21    int peek() {
+22        if(st2.empty()){
+23            while(!st1.empty()) {
+24                st2.push(st1.top());
+25                st1.pop();
+26            }
+27        }
+28
+29        return st2.top();
+30    }
+31    
+32    bool empty() {
+33        return st1.empty() && st2.empty();
+34    }
+35};
+36
+37/**
+38 * Your MyQueue object will be instantiated and called as such:
+39 * MyQueue* obj = new MyQueue();
+40 * obj->push(x);
+41 * int param_2 = obj->pop();
+42 * int param_3 = obj->peek();
+43 * bool param_4 = obj->empty();
+44 */
