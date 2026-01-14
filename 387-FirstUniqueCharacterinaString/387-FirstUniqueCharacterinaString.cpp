f41f890204e9958c09c0e3bbdeb23f8c4347460c
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-01-14 12:10:22 +0530

    File updated at 1/14/2026, 12:10:22 PM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
@@ -1,17 +1,26 @@
-// Last updated: 7/20/2025, 2:44:28 PM
-class Solution {
-public:
-    bool isPalindrome(int x) {
-        string str = to_string(x);
-
-        string str2 = str;
-
-        reverse(str2.begin(), str2.end());
-
-        if(str2 == str) {
-            return true;
-        }
-
-        return false;
-     }
-};
+// Last updated: 1/14/2026, 12:10:22 PM
+1class Solution {
+2public:
+3    int firstUniqChar(string s) {
+4        map<char, int> m;
+5        int n = s.size();
+6        queue<pair<char, int>> q;
+7        int ans = -1;
+8
+9        for(int i = 0; i < n; i++) {
+10            m[s[i]]++;
+11            q.push({s[i], i});
+12        }
+13
+14        while(!q.empty()) {
+15            if(m[q.front().first] == 1) {
+16                ans = q.front().second;
+17                break;
+18            }
+19
+20            q.pop();
+21        }
+22
+23        return ans;
+24    }
+25};
