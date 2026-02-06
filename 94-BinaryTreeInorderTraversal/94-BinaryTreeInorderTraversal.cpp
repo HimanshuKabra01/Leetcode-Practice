ab058e37e449f8178ccfcb32db8ce36ad098ec32
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-02-07 01:47:58 +0530

    File updated at 2/7/2026, 1:47:55 AM

--- 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.cpp
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/18/2026, 4:18:15 PM
+// Last updated: 2/7/2026, 1:47:55 AM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -12,21 +12,46 @@
 11 */
 12class Solution {
 13public:
-14    void inorder(TreeNode* root, vector<int>& ans) {
-15        if(root == NULL) {
-16            return;
-17        }
+14    // void inorder(TreeNode* root, vector<int>& ans) {
+15    //     if(root == NULL) {
+16    //         return;
+17    //     }
 18
-19        inorder(root->left, ans);
-20        ans.push_back(root->val);
-21        inorder(root->right, ans);
-22    }
+19    //     inorder(root->left, ans);
+20    //     ans.push_back(root->val);
+21    //     inorder(root->right, ans);
+22    // }
 23
 24    vector<int> inorderTraversal(TreeNode* root) {
 25        vector<int> ans;
 26
-27        inorder(root, ans);
+27        // inorder(root, ans);
 28
-29        return ans;
-30    }
-31};
+29        //MORRIS INORDER TRAVERSAL :-
+30
+31        TreeNode* curr = root;
+32
+33        while(curr != NULL) {
+34            if(curr->left == NULL) {
+35                ans.push_back(curr->val);
+36                curr = curr->right;
+37            } else {
+38                TreeNode* ip = curr->left;
+39                while(ip->right != NULL && ip->right != curr) {
+40                    ip = ip->right;
+41                }
+42
+43                if(ip->right == NULL) {
+44                    ip->right = curr; // create thread
+45                    curr = curr->left;
+46                } else {
+47                    ip->right = NULL; //delete thread
+48                    ans.push_back(curr->val);
+49                    curr = curr->right;
+50                }
+51            }
+52        }
+53
+54        return ans;
+55    }
+56};
