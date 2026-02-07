983f3ae49365476e899788ecd213d1b0d6ad7721
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-02-07 23:13:52 +0530

    File updated at 2/7/2026, 11:13:49 PM

--- 430-FlattenaMultilevelDoublyLinkedList/430-FlattenaMultilevelDoublyLinkedList.cpp
+++ 430-FlattenaMultilevelDoublyLinkedList/430-FlattenaMultilevelDoublyLinkedList.cpp
@@ -1,42 +1,41 @@
-// Last updated: 11/20/2025, 7:17:25 PM
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* prev;
-    Node* next;
-    Node* child;
-};
-*/
-
-class Solution {
-public:
-    Node* flatten(Node* head) {
-        if(head == NULL) return NULL;
-        
-        Node* curr = head;
-
-        while(curr != NULL) {
-            if(curr->child != NULL) {
-                Node* next = curr->next;
-                curr->next = flatten(curr->child);
-                curr->next->prev = curr;
-                curr->child = NULL;
-
-                while(curr->next != NULL) {
-                    curr = curr->next;
-                }
-
-                if(next != NULL) {
-                    curr->next = next;
-                    next->prev = curr;
-                }
-            }
-
-            curr = curr->next;
-        }
-
-        return head;
-    }
-};
+// Last updated: 2/7/2026, 11:13:49 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;  vf
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    TreeNode* helper(TreeNode* root) {
+15        if(root == NULL) {
+16            return NULL;
+17        }
+18
+19        TreeNode* leftTail = helper(root->left);
+20        TreeNode* rightTail = helper(root->right);
+21
+22        if(root->left != NULL) {
+23            TreeNode* temp = root->right;
+24            root->right = root->left;
+25            leftTail->right = temp;
+26            root->left = NULL;
+27        }
+28
+29        if(rightTail) {
+30            return rightTail;
+31        } else if(leftTail) {
+32            return leftTail;
+33        } else {
+34            return root;
+35        }
+36    }
+37    void flatten(TreeNode* root) {
+38        helper(root);
+39    }
+40};
