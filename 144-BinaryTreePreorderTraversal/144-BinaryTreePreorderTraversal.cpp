bd14a7aa9cdbb59500fb1de657ec872fed585366
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-02-07 14:49:07 +0530

    File updated at 2/7/2026, 2:49:03 PM

--- 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/18/2026, 4:24:42 PM
+// Last updated: 2/7/2026, 2:49:03 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -12,20 +12,44 @@
 11 */
 12class Solution {
 13public:
-14    void preorder(TreeNode* root, vector<int>& ans) {
-15        if(root == NULL) {
-16            return;
-17        }
+14    // void preorder(TreeNode* root, vector<int>& ans) {
+15    //     if(root == NULL) {
+16    //         return;
+17    //     }
 18
-19        ans.push_back(root->val);
-20        preorder(root->left, ans);
-21        preorder(root->right, ans);
-22    }
+19    //     ans.push_back(root->val);
+20    //     preorder(root->left, ans);
+21    //     preorder(root->right, ans);
+22    // }
 23    vector<int> preorderTraversal(TreeNode* root) {
 24        vector<int> ans;
 25
-26        preorder(root, ans);
+26        // preorder(root, ans);
 27
-28        return ans;
-29    }
-30};
+28        // MORRIS PREORDER TRAVERSAL:-
+29
+30        TreeNode* curr = root;
+31
+32        while(curr != NULL) {
+33           if(curr->left == NULL) {
+34            ans.push_back(curr->val);
+35            curr = curr->right;
+36           } else {
+37            TreeNode* ip = curr->left;
+38            while(ip->right != NULL && ip->right != curr) {
+39                ip = ip->right;
+40            }
+41            if(ip->right == NULL) {
+42                ans.push_back(curr->val);
+43                ip->right = curr;
+44                curr = curr->left;
+45            } else {
+46                ip->right = NULL;
+47                curr = curr->right;
+48            }
+49           } 
+50        }
+51
+52        return ans;
+53    }
+54};
