52189125be2760f47ddf2de3baaf68e2ee74129a
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-02-14 20:16:45 +0000

    File updated at 2/15/2026, 1:46:38 AM

--- 3838-WeightedWordMapping/3838-WeightedWordMapping.cpp
+++ 3838-WeightedWordMapping/3838-WeightedWordMapping.cpp
@@ -1,23 +1,45 @@
-// Last updated: 2/14/2026, 10:38:10 PM
+// Last updated: 2/15/2026, 1:46:38 AM
 1class Solution {
 2public:
-3    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+3    int prefixConnected(vector<string>& words, int k) {
 4        int n = words.size();
-5        string ans = "";
+5        int ans = 0;
 6
-7        for(int i = 0; i < n; i++) {
-8            string s = words[i];
-9            int sum = 0;
+7        if(n == 1) {
+8            return 0;
+9        }
 10
-11            for(int j = 0; j < s.size(); j++) {
-12                sum += weights[s[j] - 'a'];
-13            }
+11        sort(words.begin(), words.end());
+12
+13        vector<string> focus;
 14
-15            int wm = sum % 26;
-16
-17            ans += 'z' - wm;
-18        }
-19
-20        return ans;
-21    }
-22};
+15        for(int i = 0; i < n; i++) {
+16            string s = words[i];
+17
+18            if(s.size() < k) {
+19                continue;
+20            }
+21
+22            s.resize(k); 
+23
+24            focus.push_back(s);
+25        }
+26
+27        int i = 0;
+28        while(i < focus.size()) {
+29            int j = i + 1;
+30
+31            while(j < focus.size() && focus[j] == focus[i]) {
+32                j++;
+33            }
+34
+35            if(j-i >= 2) {
+36                ans++;
+37            }
+38
+39            i = j;  
+40        }
+41
+42        return ans;
+43    }
+44};
