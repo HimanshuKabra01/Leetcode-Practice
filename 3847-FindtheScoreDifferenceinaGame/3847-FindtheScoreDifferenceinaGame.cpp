ce70b380364f0e574a50bbbdff6456651a70ec0d
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-02-22 12:02:33 +0000

    File updated at 2/22/2026, 5:32:34 PM

--- 3847-FindtheScoreDifferenceinaGame/3847-FindtheScoreDifferenceinaGame.cpp
+++ 3847-FindtheScoreDifferenceinaGame/3847-FindtheScoreDifferenceinaGame.cpp
@@ -1,15 +1,15 @@
-// Last updated: 2/22/2026, 5:31:29 PM
+// Last updated: 2/22/2026, 5:32:34 PM
 1class Solution {
 2public:
 3    bool isDigitorialPermutation(int n) {
-4        unordered_map<int, int> m;
+4        vector<int> a(10);
 5
-6        m[0] = 1;
+6        a[0] = 1;
 7        int curr = 1;
 8        for(int i = 1; i <= 9; i++) {
 9            curr *= i;
 10
-11            m[i] = curr;
+11            a[i] = curr;
 12        }
 13
 14        unordered_set<string> s;
@@ -26,7 +26,7 @@
 25        while(n > 0) {
 26            int dig = n % 10;
 27
-28            sum += m[dig];
+28            sum += a[dig];
 29
 30            n /= 10;
 31        }
