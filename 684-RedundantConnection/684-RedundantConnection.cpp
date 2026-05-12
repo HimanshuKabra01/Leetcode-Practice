4b3cea1f337679147ee3db548cd1b2867e0e8f55
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-05-12 12:06:28 +0530

    File updated at 5/12/2026, 12:06:28 PM

--- 684-RedundantConnection/684-RedundantConnection.cpp
+++ 684-RedundantConnection/684-RedundantConnection.cpp
@@ -1,57 +1,36 @@
-// Last updated: 4/15/2026, 1:39:05 AM
-1/*
-2// Definition for a Node.
-3class Node {
-4public:
-5    int val;
-6    vector<Node*> neighbors;
-7    Node() {
-8        val = 0;
-9        neighbors = vector<Node*>();
-10    }
-11    Node(int _val) {
-12        val = _val;
-13        neighbors = vector<Node*>();
-14    }
-15    Node(int _val, vector<Node*> _neighbors) {
-16        val = _val;
-17        neighbors = _neighbors;
-18    }
-19};
-20*/
-21
-22class Solution {
-23public:
-24    Node* cloneGraph(Node* node) {
-25        if(node == NULL) {
-26            return NULL;
-27        }
-28
-29        queue<Node*> q;
-30        q.push(node);
-31
-32        unordered_map<Node*, Node*> m;
-33
-34        Node* newNode = new Node(node->val);
-35        m[node] = newNode;
-36
-37        while(!q.empty()) {
-38            Node* curr = q.front();
-39            q.pop();
-40
-41            for(auto x : curr->neighbors) {
-42                if(!m.count(x)) { 
-43                    q.push(x);
-44
-45                    Node* nNode = new Node(x->val);
-46
-47                    m[x] = nNode;
-48                }
-49
-50                m[x]->neighbors.push_back(m[curr]);
-51            }
-52        }
-53
-54        return newNode;
-55    }
-56};
+// Last updated: 5/12/2026, 12:06:28 PM
+1class Solution {
+2public:
+3    void dfs(vector<vector<int>> &adj, vector<bool> &vis, int u) {
+4        vis[u] = true;
+5
+6        for(int v : adj[u]) {
+7            if(!vis[v]) {
+8                dfs(adj, vis, v);
+9            }
+10        }
+11    }
+12    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+13        int V = edges.size();
+14
+15        vector<vector<int>> adj(V+1);
+16        vector<int> ans;
+17
+18        for(int i = 0; i < V; i++) {
+19            vector<bool> vis(V+1, false);
+20
+21            dfs(adj, vis, edges[i][0]);
+22
+23            if(!vis[edges[i][1]]) {
+24                adj[edges[i][0]].push_back(edges[i][1]);
+25                adj[edges[i][1]].push_back(edges[i][0]);
+26            } else {
+27                ans.push_back(edges[i][0]);
+28                ans.push_back(edges[i][1]);
+29                break;
+30            }
+31        }
+32
+33        return ans; 
+34    }
+35};
