5daba2c8ce7feff5511c8883b8abb1f45b1c82e8
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-06-15 01:21:40 +0530

    File updated at 6/15/2026, 1:21:40 AM

--- 130-SurroundedRegions/130-SurroundedRegions.cpp
+++ 130-SurroundedRegions/130-SurroundedRegions.cpp
@@ -1,77 +1,36 @@
-// Last updated: 6/14/2026, 9:06:49 PM
+// Last updated: 6/15/2026, 1:21:40 AM
 1class Solution {
 2public:
-3    int numEnclaves(vector<vector<int>>& grid) {
-4        int m = grid.size();
-5        int n = grid[0].size();
-6
-7        queue<pair<int, int>> q;
-8
-9        for(int i = 0; i < n; i++) {
-10            if(grid[0][i] == 1) {
-11                q.push({0, i});
-12                grid[0][i] = 2;
-13            }
-14
-15            if(grid[m-1][i] == 1) {
-16                q.push({m-1, i});
-17                grid[m-1][i] = 2;
+3    void dfs(vector<vector<int>> &graph, int u, bool &ans, vector<int> &vis, int col) {
+4        if(!ans) return;
+5
+6        int op;
+7        if(col == 1) op = 0;
+8        else if(col == 0) op = 1;
+9
+10        vis[u] = op;
+11
+12        for(int v : graph[u]) {
+13            if(vis[v] == -1) {
+14                dfs(graph, v, ans, vis, vis[u]);
+15            } else if(vis[v] == op) {
+16                ans = false;
+17                return;
 18            }
 19        }
-20
-21        for(int i = 0; i < m; i++) {
-22            if(grid[i][0] == 1) {
-23                q.push({i, 0});
-24                grid[i][0] = 2;
-25            }
-26
-27            if(grid[i][n-1] == 1) {
-28                q.push({i, n-1});
-29                grid[i][n-1] = 2;
-30            }
-31        }
-32
-33        while(!q.empty()) {
-34            int k = q.size();
-35
-36            for(int l = 0; l < k; l++) {
-37                int i = q.front().first;
-38                int j = q.front().second;
-39
-40                q.pop();
-41
-42                if(i-1 >= 0 && grid[i-1][j] == 1) {
-43                    q.push({i-1, j});
-44                    grid[i-1][j] = 2;
-45                }
-46
-47                if(j+1 < n && grid[i][j+1] == 1) {
-48                    q.push({i, j+1});
-49                    grid[i][j+1] = 2;
-50                }
-51
-52                if(i+1 < m && grid[i+1][j] == 1) {
-53                    q.push({i+1, j});
-54                    grid[i+1][j] = 2;
-55                }
-56
-57                if(j-1 >= 0 && grid[i][j-1] == 1) {
-58                    q.push({i, j-1});
-59                    grid[i][j-1] = 2;
-60                }
-61            }
-62        }
-63
-64        int ans = 0;
-65
-66        for(int i = 0; i < m; i++) {
-67            for(int j = 0; j < n; j++) {
-68                if(grid[i][j] == 1) {
-69                    ans++;
-70                }
-71            }
-72        }
-73
-74        return ans;
-75    }
-76};
+20    }
+21    bool isBipartite(vector<vector<int>>& graph) {
+22        int V = graph.size();
+23
+24        bool ans = true;
+25        vector<int> vis(V, -1);
+26        for(int i = 0; i < V; i++) {
+27            if(vis[i] != -1) continue;
+28
+29            dfs(graph, i, ans, vis, 1);
+30        }
+31
+32        if(!ans) return false;
+33        return true;
+34    }
+35};
