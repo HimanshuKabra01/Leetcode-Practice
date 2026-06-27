310644827f2c6beb8cf1ca82473f718fecdd7026
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-06-28 01:59:57 +0530

    File updated at 6/28/2026, 1:59:58 AM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -1,36 +1,82 @@
-// Last updated: 1/16/2026, 11:31:39 AM
-1class Solution {
-2public:
-3    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-4       int n = nums.size();
-5
-6        deque<int> dq;
-7        vector<int> ans;
-8
-9        for(int i = 0; i < k; i++) {
-10          while(!dq.empty() && nums[dq.back()] <= nums[i]) {
-11            dq.pop_back();
-12          }
+// Last updated: 6/28/2026, 1:59:58 AM
+1class DisjointSet {
+2  vector<int> rank, parent, size;
+3public:
+4  DisjointSet(int n) {
+5    rank.resize(n+1, 0);
+6    parent.resize(n+1);
+7    size.resize(n+1);
+8    for(int i = 0; i <= n; i++) {
+9      parent[i] = i;
+10      size[i] = 1;
+11    }
+12  }
 13
-14          dq.push_back(i); 
-15        }
-16
-17        for(int i = k; i < n; i++) {
-18            ans.push_back(nums[dq.front()]);
+14  // PATH COMPRESSION - CHANGES COMPLEXITY FROM LOG N TO CONSTANT
+15  int findUPar(int u) {
+16    if(u == parent[u]) {
+17      return u;
+18    }
 19
-20            while(!dq.empty() && dq.front() <= i - k) {
-21                dq.pop_front();
-22            }
-23
-24            while(!dq.empty() && nums[dq.back()] <= nums[i]) {
-25                dq.pop_back();
-26            }
-27
-28            dq.push_back(i);
-29        }
-30
-31        ans.push_back(nums[dq.front()]);
-32
-33        return ans;
-34    }
-35};
+20    return parent[u] = findUPar(parent[u]);
+21  }
+22
+23  void unionByRank(int u, int v) {
+24    int ulU = findUPar(u);
+25    int ulV = findUPar(v);
+26
+27    if(ulU == ulV) return;
+28
+29    if(rank[ulU] < rank[ulV]) {
+30      parent[ulU] = ulV;
+31    } else if(rank[ulV] < rank[ulU]) {
+32      parent[ulV] = ulU;
+33    } else {
+34      parent[ulV] = ulU;
+35      rank[ulU]++;
+36    }
+37  }
+38
+39  void unionBySize(int u, int v) {
+40    int ulU = findUPar(u);
+41    int ulV = findUPar(v);
+42
+43    if(ulU == ulV) return;
+44
+45    if(size[ulU] < size[ulV]) {
+46      parent[ulU] = ulV;
+47      size[ulU] += size[ulV];
+48    } else {
+49      parent[ulV] = ulU;
+50      size[ulV] += size[ulU];
+51    }
+52  }
+53};
+54
+55class Solution {
+56public:
+57    int makeConnected(int n, vector<vector<int>>& connections) {
+58        if(connections.size() < n-1) {
+59            return -1;
+60        }
+61
+62        DisjointSet ds(n);
+63
+64        for(int i = 0; i < connections.size(); i++) {
+65            ds.unionBySize(connections[i][0], connections[i][1]);
+66        }
+67
+68        int dis = 0;
+69        for(int i = 0; i < n; i++) {
+70            if(ds.findUPar(i) == i) {
+71                dis++;
+72            }
+73        }
+74
+75        if(dis == 1) {
+76            return 0;
+77        }
+78
+79        return dis-1;
+80    }
+81};
