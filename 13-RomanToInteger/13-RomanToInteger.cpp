f822e7beb03fe9475f98acebfc940ca576fabd21
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-08-09 12:43:14 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.cpp
@@ -0,0 +1,27 @@
+// Last updated: 8/9/2026, 12:43:13 PM
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> mp;
+
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+
+        int ans = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+            if(i < s.size()-1 && mp[s[i]] < mp[s[i+1]]) {
+                ans -= mp[s[i]];
+            } else {
+                ans += mp[s[i]];
+            }
+        }
+
+        return ans;
+    }
+};
