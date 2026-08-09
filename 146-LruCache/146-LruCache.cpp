256fc2c020a3af0056d04ce1a08c24150c517e30
Himanshu Kabra <kabrahimanshu123@gmail.com>
2026-08-09 12:39:57 +0530

    Create 146-LruCache

---
+++ 146-LruCache/146-LruCache.cpp
@@ -0,0 +1,88 @@
+// Last updated: 8/9/2026, 12:39:57 PM
+class LRUCache {
+public:
+    class Node {
+    public:
+        int key, val;
+
+        Node* next;
+        Node* prev;
+
+        Node(int k, int v) {
+            key = k;
+            val = v;
+
+            next = prev = NULL;
+        }
+    };
+
+    Node* head = new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
+    
+    unordered_map<int, Node*> m;
+    int limit;
+
+    void addNode(Node* newNode) {
+        Node* temp = head->next;
+
+        head->next = newNode;
+        temp->prev = newNode;
+
+        newNode->next = temp;
+        newNode->prev = head;
+    }
+
+    void delNode(Node* oldNode) {
+        Node* oldNext = oldNode->next;
+        Node* oldPrev = oldNode->prev;
+
+        oldPrev->next = oldNext;
+        oldNext->prev = oldPrev;
+    }
+
+    LRUCache(int capacity) {
+        limit = capacity;
+
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if(m.find(key) == m.end()) return -1;
+
+        Node* ansNode = m[key];
+        int ans = ansNode->val;
+
+        m.erase(key);
+        delNode(ansNode);
+
+        addNode(ansNode);
+        m[key] = ansNode;
+
+        return ans;
+    }
+    
+    void put(int key, int value) {
+        if(m.find(key) != m.end()) {
+            Node* oldNode = m[key];
+            delNode(oldNode);
+            m.erase(key);
+        }
+
+        if(m.size() == limit) {
+            m.erase(tail->prev->key);
+            delNode(tail->prev);
+        }
+
+        Node* newNode = new Node(key, value);
+        addNode(newNode);
+        m[key] = newNode;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
